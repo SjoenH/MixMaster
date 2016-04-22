500255278deeb493c0c5dd41ebf466488f74980b
Henry Skorpe Sjøen <henryskorpe@gmail.com>
2016-04-22 19:35:25 +0200

    Support for multiple users
    Beware of spaghetticode

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var channelList = [];
+var clientList = [];
+var userObjects = [];
+
+var dev = true;
+if (dev) {
+  channelList = ['Kick','Snare','Rack-Tom','Floor-Tom','Hi-hat','OH','Bass','Guitar','Keys'];
+}
+// for (var i = 0; i < demoChannels.length; i++) {
+//    socket.emit('newChannel',demoChannels[i]);
+// }
+
 app.get('/', function(req, res){
   res.sendfile('client.html');
 });
@@ -10,9 +22,38 @@ app.get('/host', function(req, res){
   res.sendfile('host.html');
 });
 
+app.get('/admin', function(req,res){
+  res.sendFile('admin.html');
+});
+
 io.on('connection', function(socket){
   console.log('a user connected');
-  // Add to list of connected clients
+  socket.on('userSendsID',function(userID){
+    if (userID == 'admin') {
+        socket.emit('allChannels',channelList);
+    }else {
+      //if new userID
+      // Add to list of connected clients
+      console.log(userID);
+      if(clientList.indexOf(userID)==-1){
+        //Add channels to user.
+        var newPerson = {Name:userID};
+        clientList.push(userID);
+        userObjects.push(newPerson);
+        //Send all available channels
+        socket.emit('allChannels',channelList);
+        console.log(clientList);
+        //send new user to host
+        io.emit('newUser',channelList,userID);
+      }else {
+        //send the users existing channels
+        console.log("User already exists!");
+      };
+      console.log("clientList.length: ",clientList.length);
+    };
+  });
+
+  //on disconnect:
   socket.on('disconnect', function(){
     console.log('user disconnected');
     // Remove from list of connected clients
@@ -20,21 +61,19 @@ io.on('connection', function(socket){
   socket.on('newChannel', function(channelName){
     console.log('New Channel: ' + channelName);
     // Add to list of available channels
+    console.log(io.clients);
+    channelList.push(channelName);
+    console.log(channelList);
     // Send to all connected clients
     io.emit('newChannel', channelName);
   });
-  socket.on('newUser',function(userName){
-    console.log("New User: "+ userName);
-    // Send to all connected clients
-    io.emit('newUser',userName);
-  });
-  socket.on('setChannelValue',function(channelId,channelValue){
+  socket.on('setChannelValue',function(userID,channelId,channelValue){
+    console.log("User: ",userID);
     console.log("channelId: "+channelId);
     console.log("channelValue: "+ channelValue);
-
     // Send to master view
-    io.emit('setChannelValue',channelId,channelValue);
-  })
+    io.emit('setChannelValue',userID,channelId,channelValue);
+  });
 });
 
 http.listen(3000, function(){
